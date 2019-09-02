11e0d7c2f0bef46ada5797301480a55db01339d0
Samuel Hoelzl <Samuel.Hoelzl@world-direct.at>
2019-09-02 20:58:45 +0200

    floating button

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,19 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class Todo {
+  String title;
+  bool checked;
+}
+
+  class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: TodoList(title: 'Todos'),
+      home: TodoList(title: 'To Do\'s'),
     );
   }
 }
@@ -21,25 +25,20 @@ class TodoList extends StatefulWidget {
   final String title;
 
   @override
-  _TodoListState createState() => _TodoListState();
+  _TodoListState createState() => new _TodoListState();
 }
 
-class Todo {
-  String title;
-  bool checked;
-}
 
-class _TodoListState extends State<TodoList> {
-  final _todos = <Todo>[];
+class  _TodoListState extends State<TodoList> {
+  var _todos = <Todo>[];
   final _biggerFont = const TextStyle(fontSize: 20.0);
   String _textFieldValue;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(widget.title)),
-      body: _buildContainer(),
-    );
+        appBar: AppBar(title: Text(widget.title)),
+        body: _buildContainer());
   }
 
   Widget _buildContainer() {
@@ -48,7 +47,7 @@ class _TodoListState extends State<TodoList> {
   }
 
   Widget _buildList() {
-    return new Expanded(
+    return Expanded(
         child: ListView.builder(
             padding: const EdgeInsets.all(1),
             itemCount: _todos.length,
@@ -57,23 +56,37 @@ class _TodoListState extends State<TodoList> {
             }));
   }
 
+
+  Widget _buildTodoRow(Todo todo) {
+    return ListTile(
+      title: Text(todo.title, style: _biggerFont),
+      trailing:
+      Icon(todo.checked ? Icons.check_box : Icons.check_box_outline_blank),
+      onTap: () {
+        setState(() {
+          todo.checked = !todo.checked;
+        });
+      },
+    );
+  }
+
   Widget _buildAddTodoRow() {
     return Row(children: [_buildTextField(), _buildAddTodoButton()]);
   }
 
   Widget _buildAddTodoButton() {
-    return new Padding(
+    return Padding(
         padding: EdgeInsets.all(10),
-        child: RaisedButton(
+        child: FloatingActionButton(
             onPressed: () {
               setState(() {
-                Todo todo = new Todo();
+                Todo todo = Todo();
                 todo.title = _textFieldValue;
                 todo.checked = false;
                 _todos.add(todo);
               });
             },
-            child: const Text('Add', style: TextStyle(fontSize: 20))));
+            child: const Icon(Icons.add)));
   }
 
   Widget _buildTextField() {
@@ -85,16 +98,4 @@ class _TodoListState extends State<TodoList> {
             })));
   }
 
-  Widget _buildTodoRow(Todo todo) {
-    return ListTile(
-      title: Text(todo.title, style: _biggerFont),
-      trailing:
-          Icon(todo.checked ? Icons.check_box : Icons.check_box_outline_blank),
-      onTap: () {
-        setState(() {
-          todo.checked = !todo.checked;
-        });
-      },
-    );
-  }
 }
